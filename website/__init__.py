682538c17f8d120e86267fd477b7020da6e9fedd
Sanjay S <scsewbh@gmail.com>
2021-02-25 15:14:28 -0500

    v0.5 - Shell Ready

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
+import os
 #INIT FILE - is called main.py and is the setup for the creation of the web app
 def create_app():
     app = Flask(__name__)
-    app. config['SECRET_KEY'] = 'MCS_secret'
-
+    app.config['SECRET_KEY'] = os.environ.get("SECRET_FLASK", None)
 
     from .views import views
     from .auth import auth
