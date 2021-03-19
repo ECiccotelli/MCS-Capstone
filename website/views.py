489eed438e8f1326d3214e50c0719092e7fc568a
Sanjay S <scsewbh@gmail.com>
2021-03-18 21:18:47 -0400

    v0.9 - Add/Remove/Conflict Javascript

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, session
+from flask import Blueprint, render_template, redirect, url_for, session, request
 
 from google.cloud import datastore
 import os
@@ -10,15 +10,31 @@ views = Blueprint('views', __name__)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'website\static\secret\mc-scheduleMaker-ee3731698260.json'
 client = datastore.Client()
 
-@views.route('/')
+@views.route('/', methods = ['GET', 'POST'])
 def home():
-    print(session)
-    if 'authenticated' in session and session['authenticated']:
-        query = client.query(kind="Courses")
-        results = list(query.fetch())
-        return render_template('index.html', userid=session['userid'], name=session['name'], image=session['image'], courses=results)
+    if request.method == 'POST':
+        key = client.key('UserCourses', session['userid'])
+        entity = datastore.Entity(key=key)
+        entity.update({
+            'MyClassList': request.form["mylistdata"]
+        })
+        client.put(entity)
+        print(request.form["mylistdata"])
+        return redirect(url_for('views.home'))
     else:
-        return redirect(url_for('auth.login')) #ERROR or SIGN IN FIRST
+        print(session)
+        if 'authenticated' in session and session['authenticated']:
+            query = client.query(kind="Courses")
+            results = list(query.fetch())
+            query = client.query(kind="UserCourses") #DOESNT WORK GRABS ALL USERS
+            userlist = list(query.fetch())
+            dictResults = eval(userlist[0]['MyClassList'])
+            print('ea')
+            print(dictResults)
+            return render_template('index.html', userid=session['userid'], name=session['name'], image=session['image'], courses=results, mylist=dictResults)
+        else:
+            return redirect(url_for('auth.login')) #ERROR or SIGN IN FIRST
+
 
 @views.route('/registration')
 def registration():
